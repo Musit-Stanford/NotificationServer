2df65132209bc2d28a2650d9271bf77bdc515d29
Juan Marroquin <juam95@stanford.edu>
2017-03-10 02:27:47 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var express = require('express')
 var app = express()
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./musit-ebe2c-firebase-adminsdk-usxes-34888db635.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://musit-ebe2c.firebaseio.com"
+});
+
+var db = admin.database()
+var ref = db.ref('messages')
+
+ref.orderByChild('createdAt').endAt(Date.now()).on('child_added', function(snapshot) {
+  console.log('new record', snapshot.key);
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
